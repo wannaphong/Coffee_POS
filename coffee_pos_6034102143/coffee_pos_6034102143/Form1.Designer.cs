a4ac57f18b47b773e6da0c75d7b0bf8905ef5a33
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-11-10 11:29:05 +0700

    add Control tabpage
    เพิ่มโค้ดต้นแบบควบคุมการโชว์  tabpage

--- coffee_pos_6034102143/coffee_pos_6034102143/Form1.Designer.cs
+++ coffee_pos_6034102143/coffee_pos_6034102143/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Payment = new System.Windows.Forms.TabPage();
+            this.ok_pay = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -41,14 +47,60 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Coffee POS";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.Payment);
+            this.tabControl1.Location = new System.Drawing.Point(12, 109);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(756, 192);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Controls.Add(this.ok_pay);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(748, 166);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Payment
+            // 
+            this.Payment.Location = new System.Drawing.Point(4, 22);
+            this.Payment.Name = "Payment";
+            this.Payment.Padding = new System.Windows.Forms.Padding(3);
+            this.Payment.Size = new System.Drawing.Size(748, 166);
+            this.Payment.TabIndex = 1;
+            this.Payment.Text = "Payment";
+            this.Payment.UseVisualStyleBackColor = true;
+            // 
+            // ok_pay
+            // 
+            this.ok_pay.Location = new System.Drawing.Point(300, 78);
+            this.ok_pay.Name = "ok_pay";
+            this.ok_pay.Size = new System.Drawing.Size(75, 23);
+            this.ok_pay.TabIndex = 0;
+            this.ok_pay.Text = "OK";
+            this.ok_pay.UseVisualStyleBackColor = true;
+            this.ok_pay.Click += new System.EventHandler(this.ok_pay_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 293);
+            this.ClientSize = new System.Drawing.Size(780, 313);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Coffee POS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +109,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage Payment;
+        private System.Windows.Forms.Button ok_pay;
     }
 }
 
--- coffee_pos_6034102143/coffee_pos_6034102143/Form1.cs
+++ coffee_pos_6034102143/coffee_pos_6034102143/Form1.cs
@@ -12,9 +12,26 @@ namespace coffee_pos_6034102143
 {
     public partial class Form1 : Form
     {
+        List<Tuple<double, string>> product = new List<Tuple<double, string>>();
+        bool ok=false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabControl1.Controls.Remove(Payment); // ลบ Payment ออกจาก tabControl1
+        }
+
+        private void ok_pay_Click(object sender, EventArgs e)
+        {
+            if (ok == false) {
+                tabControl1.Controls.Add(Payment); // เพิ่ม Payment
+                ok = true;
+                tabControl1.SelectedTab = Payment; // กำหนดให้เลือกTab ชื่อ Payment มาแสดงผล
+            }
+            else tabControl1.SelectedTab = Payment; // กำหนดให้เลือกTab ชื่อ Payment มาแสดงผล
         }
     }
 }
