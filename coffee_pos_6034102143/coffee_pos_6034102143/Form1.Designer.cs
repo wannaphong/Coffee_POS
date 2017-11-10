f3f5b36f44b237b0978475c7f12f876f860b423b
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2017-11-10 11:38:41 +0700

    add About and new set size
    เพิ่ม About และ ปรับขนาดใหม่

--- coffee_pos_6034102143/coffee_pos_6034102143/Form1.Designer.cs
+++ coffee_pos_6034102143/coffee_pos_6034102143/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.Payment = new System.Windows.Forms.TabPage();
             this.ok_pay = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -51,10 +52,11 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.Payment);
+            this.tabControl1.Controls.Add(this.About);
             this.tabControl1.Location = new System.Drawing.Point(12, 109);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(756, 192);
+            this.tabControl1.Size = new System.Drawing.Size(801, 414);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
@@ -64,7 +66,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(748, 166);
+            this.tabPage1.Size = new System.Drawing.Size(793, 388);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -81,7 +83,7 @@
             // 
             // ok_pay
             // 
-            this.ok_pay.Location = new System.Drawing.Point(300, 78);
+            this.ok_pay.Location = new System.Drawing.Point(155, 330);
             this.ok_pay.Name = "ok_pay";
             this.ok_pay.Size = new System.Drawing.Size(75, 23);
             this.ok_pay.TabIndex = 0;
@@ -89,11 +91,21 @@
             this.ok_pay.UseVisualStyleBackColor = true;
             this.ok_pay.Click += new System.EventHandler(this.ok_pay_Click);
             // 
+            // About
+            // 
+            this.About.Location = new System.Drawing.Point(4, 22);
+            this.About.Name = "About";
+            this.About.Padding = new System.Windows.Forms.Padding(3);
+            this.About.Size = new System.Drawing.Size(793, 388);
+            this.About.TabIndex = 2;
+            this.About.Text = "About";
+            this.About.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 313);
+            this.ClientSize = new System.Drawing.Size(1026, 546);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -113,6 +125,7 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage Payment;
         private System.Windows.Forms.Button ok_pay;
+        private System.Windows.Forms.TabPage About;
     }
 }
 
